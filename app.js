2292cddeb4107f57b42be1590515aebcb7f2ad68
Robin Ciurlik <robin.ciurlik@gmail.com>
2024-10-08 15:33:52 +0200

    Ajout de la table de jeu

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+
+
+
+
+
+
+
+
 const express = require('express');
 const path = require('path');
 const app = express();
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,12 +3,18 @@
     <h1>Home</h1>
     <p>Ceci est la page home.</p>
   </div>
+  <div>
+    <ul id="message"></ul>
+    <input id="m"/>
+    <button onclick="send()">send</button>
+  </div>
 </template>
 
-<script>
+<script >
 export default {
   name: 'HomePage',
 }
+
 </script>
 
 <style>
@@ -19,4 +25,6 @@ nav {
 nav a {
   margin: 0 10px;
 }
+
+
 </style>
