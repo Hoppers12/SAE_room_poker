8f11848a607455501c479260b5dcd3365de203e9
Timothée <timothee.laumonier@gmail.com>
2024-10-09 16:41:32 +0200

    change

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const User = require('./models/user');
 const {join} = require("node:path");
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
