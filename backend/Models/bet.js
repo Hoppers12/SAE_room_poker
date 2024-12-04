56a1e18e319ab54c12eef8e83665aaabb0d7c06f
Timothée <timothee.laumonier@gmail.com>
2024-12-04 13:52:27 +0100

    Page pari améliorée

--- backend/Models/bet.js
+++ backend/Models/bet.js
@@ -4,13 +4,12 @@ const mongoose = require('mongoose');
 const betSchema = new mongoose.Schema({
     bet_date:{type:Date},
     bet_expire_date: { type: Date, default: () => new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) },
-    bet_odds:[{type: mongoose.Schema.Types.ObjectId, ref:'Odd'}],
     team:{type:mongoose.Schema.Types.ObjectId, ref: 'Team'},
     bet_result: { type: String},
     sport: [{type:mongoose.Schema.Types.ObjectId, ref: 'Sport'}],
     matches: [{type:mongoose.Schema.Types.ObjectId, ref: 'Match'}],
 });
 
-
 const Bet = mongoose.model('Bet', betSchema);
 module.exports = Bet;
+
--- backend/Models/match.js
+++ backend/Models/match.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Team = require('./team');
 const Sport = require('./sports');
-
+const Bet = require('./bet');
 const matchSchema = new mongoose.Schema({
     home_team: { type: mongoose.Schema.Types.ObjectId, ref: 'Team' },
     away_team: { type: mongoose.Schema.Types.ObjectId, ref: 'Team' },
@@ -9,34 +9,82 @@ const matchSchema = new mongoose.Schema({
     result: { type: String, default: null },
     id_sport: { type: mongoose.Schema.Types.ObjectId, ref: 'Sport' },
     match_date: { type: Date, default: Date.now },
+    odds: [{home : {type: Number}, draw : {type: Number}, away : {type: Number}}],
+
 });
 
 const Match = mongoose.model('Match', matchSchema);
 
+
+
 async function insertDefaultMatches() {
     const count = await Match.countDocuments();
     if (count === 0) {
-
         const teams = await Team.find();
         const sports = await Sport.find();
         const teamMap = {};
         const sportMap = {};
-
         teams.forEach(team => {
             teamMap[team.name] = team._id;
         });
-
         sports.forEach(sport => {
             sportMap[sport.name] = sport._id;
         });
-
         const defaultMatches = [
-            { home_team: teamMap['PSG'], away_team: teamMap['OL'],match_name : 'PSG-OL', result: null, id_sport: sportMap['Football'], match_date: new Date('2025-10-01') },
-            { home_team: teamMap['PSG'], away_team: teamMap['OL'],match_name : 'PSG-OL(test)', result: null, id_sport: sportMap['Football'], match_date: new Date('2024-01-01') },
-            { home_team: teamMap['FC Barcelone'], away_team: teamMap['Manchester United'],match_name : 'Barça-Manchester', result: null, id_sport: sportMap['Football'], match_date: new Date('2024-10-02') },
-            { home_team: teamMap['Novak Djokovic'], away_team: teamMap['Rafael Nadal'],match_name : 'Novak-Nadal', result: null, id_sport: sportMap['Tennis'], match_date: new Date('2025-10-03') },
-            { home_team: teamMap['Stade Toulousain'], away_team: teamMap['Leinster Rugby'],match_name : 'Stade toulousain-Leinster', result: null, id_sport: sportMap['Rugby'], match_date: new Date('2025-10-04') },
-            { home_team: teamMap['Los Angeles Lakers'], away_team: teamMap['Chicago Bulls'],match_name : 'Lakers-bulls', result: null, id_sport: sportMap['Basketball'], match_date: new Date('2025-10-05') },
+            {
+                home_team: teamMap['PSG'],
+                away_team: teamMap['OL'],
+                match_name: 'PSG-OL',
+                result: null,
+                id_sport: sportMap['Football'],
+                match_date: new Date('2025-10-01'),
+                odds: [{ home: 1.8, draw: 3.5, away: 4.0 }]
+            },
+            {
+                home_team: teamMap['PSG'],
+                away_team: teamMap['OL'],
+                match_name: 'PSG-OL(test)',
+                result: null,
+                id_sport: sportMap['Football'],
+                match_date: new Date('2024-01-01'),
+                odds: [{ home: 1.9, draw: 3.2, away: 3.8 }]
+            },
+            {
+                home_team: teamMap['FC Barcelone'],
+                away_team: teamMap['Manchester United'],
+                match_name: 'Barça-Manchester',
+                result: null,
+                id_sport: sportMap['Football'],
+                match_date: new Date('2024-10-02'),
+                odds: [{ home: 2.1, draw: 3.1, away: 3.6 }]
+            },
+            {
+                home_team: teamMap['Novak Djokovic'],
+                away_team: teamMap['Rafael Nadal'],
+                match_name: 'Novak-Nadal',
+                result: null,
+                id_sport: sportMap['Tennis'],
+                match_date: new Date('2025-10-03'),
+                odds: [{ home: 1.7, draw: null, away: 2.2 }]
+            },
+            {
+                home_team: teamMap['Stade Toulousain'],
+                away_team: teamMap['Leinster Rugby'],
+                match_name: 'Stade toulousain-Leinster',
+                result: null,
+                id_sport: sportMap['Rugby'],
+                match_date: new Date('2025-10-04'),
+                odds: [{ home: 2.3, draw: 3.5, away: 2.9 }]
+            },
+            {
+                home_team: teamMap['Los Angeles Lakers'],
+                away_team: teamMap['Chicago Bulls'],
+                match_name: 'Lakers-Bulls',
+                result: null,
+                id_sport: sportMap['Basketball'],
+                match_date: new Date('2025-10-05'),
+                odds: [{ home: 1.5, draw: null, away: 2.7 }]
+            },
         ];
 
         await Match.insertMany(defaultMatches);
